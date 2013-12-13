4e43a99d18e7a3b4e7c413cb3af03e84ca5a17f6
Karel <ke4@sanger.ac.uk>
2013-12-13 14:46:07 +0000

    adding namespace definition

--- lib/lims-exception-notifier-app.rb
+++ lib/lims-exception-notifier-app.rb
@@ -1,2 +1,8 @@
 require 'lims-exception-notifier-app/version'
 require 'lims-exception-notifier-app/exception_notifier'
+
+# Defining the application's namespace
+module Lims
+  module ExceptionNotifierApp
+  end
+end
